f03bb25aaf0ea0c21255bec6aa31621e941147d0
Harinder <96901742+harinder007@users.noreply.github.com>
2023-07-06 23:35:21 +0530

    Update Task1(a1).js

--- Array Function/Task1(a1).js
+++ Array Function/Task1(a1).js
@@ -3,7 +3,7 @@ const numbers=[2,5,10,12,18,22];
 let x=11;
 let num1= numbers.find(function(n){
     return n>x;
- });
+ }); 
  console.log("find = ",num1); 
  let num2=numbers.findIndex(function(n){
     return n>x;
